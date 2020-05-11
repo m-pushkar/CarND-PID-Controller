8eace9b2cbd1c9f14577a57dafe784d9414373a5
Pushkar Mehendale <mehenpm@gmail.com>
2020-05-11 10:10:57 -0500

    Initial commit

---
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -0,0 +1,30 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+    // Initialize errors
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+    double prev_cte = 0;
+    prev_cte = p_error;
+    
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - prev_cte;
+}
+
+double PID::TotalError() {
+    double PID = -p_error * Kp - i_error * Ki - d_error * Kd;
+    return PID; 
+}
