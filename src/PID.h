e7885a1179799faeb15bd16a9fcef1f30ce4ac4c
Pushkar Mehendale <mehenpm@gmail.com>
2020-05-12 13:17:37 -0500

    style: restructure folders

--- src/PID.h
+++ src/PID.h
@@ -30,9 +30,8 @@ class PID {
     
     /**
      * Initialize PID.
-     * @param (Kp_, Ki_, Kd_) The initial PID coefficients
      */
-    void Init(double Kp_, double Ki_, double Kd_);
+    void Init(double Kp, double Ki, double Kd);
     
     /**
      * Update the PID error variables given cross track error.
